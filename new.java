c47a7e1fb070987c1107b0f2533d297d271d4384
tanmayagarwal06 <tanmayagarwal125@gmail.com>
2025-08-18 23:44:36 +0530

    Correction

--- new.java
+++ new.java
@@ -1,6 +1,6 @@
 public class New {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        sysout.println("This is a new Java file.");
+        System.out.println("This is a new Java file.");
     }
 }
