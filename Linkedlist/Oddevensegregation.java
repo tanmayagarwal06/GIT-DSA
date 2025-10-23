99d35488cbde16b01dc63b843746cd7bdeff6452
tanmayagarwal06 <tanmayagarwal125@gmail.com>
2025-10-23 22:13:38 +0530

    leetcode 328

---
+++ Linkedlist/Oddevensegregation.java
@@ -0,0 +1,25 @@
+package Linkedlist;
+
+import java.lang.classfile.components.ClassPrinter.ListNode;
+
+public class Oddevensegregation {
+    public Node oddEvenList(Node head) {
+        if (head == null || head.next == null) return head;
+        
+        Node odd = head;
+        Node even = head.next;
+        Node evenHead = even;
+
+        while (even != null && even.next != null) {
+            odd.next = even.next;
+            odd = odd.next;
+
+            even.next = odd.next;
+            even = even.next;
+        }
+
+        odd.next = evenHead;
+        return head;
+    }
+
+}
