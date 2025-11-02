87a9e9c1d27d785fb3ae798540bac80d5114f0d2
tanmayagarwal06 <tanmayagarwal125@gmail.com>
2025-11-02 10:09:14 +0530

    Square root search using binary search

---
+++ Arrays/BinarySearch/Squrareroot.java
@@ -0,0 +1,25 @@
+package Arrays.BinarySearch;
+
+public class Squrareroot {
+    
+    public static int sqrt(int target){
+
+        int left = 0, right = target, ans = 0;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            long sq = 1L * mid * mid;
+
+            if (sq == target) {
+                return mid;
+            } else if (sq < target) {
+                ans = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+}
