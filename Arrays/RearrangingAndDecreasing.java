a42fcadd84d4aa31c0706c5fefe2a9339a8236d5
tanmayagarwal06 <tanmayagarwal125@gmail.com>
2026-06-29 00:51:22 +0530

    Maximum Element After Decreasing and Rearranging

---
+++ Arrays/RearrangingAndDecreasing.java
@@ -0,0 +1,16 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class RearrangingAndDecreasing {
+    public int maximumElementAfterDecrementingAndRearranging(int[] arr){
+        Arrays.sort(arr);
+
+        arr[0] = 1;
+        for(int i = 1; i < arr.length; i++){
+            arr[i] = Math.min(arr[i], arr[i-1] + 1);
+        }
+        return arr[arr.length-1];
+        
+    }
+}
